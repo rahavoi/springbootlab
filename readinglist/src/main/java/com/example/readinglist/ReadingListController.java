12e51787c60565359b062c1bd2cb75a4cac96ee8
Illia Rahavoi <irahavoi@bbmtek.com>
2017-07-20 16:25:22 -0400

    Added simple controller and UI

---
+++ readinglist/src/main/java/com/example/readinglist/ReadingListController.java
@@ -0,0 +1,42 @@
+package com.example.readinglist;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class ReadingListController {
+	private ReadingListRepository readingListRepository;
+	
+	@Autowired
+	public ReadingListController(ReadingListRepository readingListRepository) {
+		this.readingListRepository = readingListRepository;
+	}
+	
+	@RequestMapping(value="/{reader}", method = RequestMethod.GET)
+	public String getReadingList(@PathVariable String reader, Model model) {
+		List<Book> books = readingListRepository.findByReader(reader);
+		
+		if(books != null) {
+			model.addAttribute("books", books);
+		}
+		
+		return "readinglist";
+	}
+	
+	@RequestMapping(value = "/{reader}", method = RequestMethod.POST)
+	public String addToReadingList(@PathVariable String reader, Book book){
+		book.setReader(reader);
+		
+		readingListRepository.save(book);
+		
+		return "redirect:/{reader}";
+	}
+	
+}
